f23f313f41ecdecc465e01055c87333d57fc8d7d
kulasin <70981278+kulasin@users.noreply.github.com>
2025-03-03 02:38:14 +0100

    laravel-service

---
+++ app-service-laravel/resources/views/layouts/layouts-horizontal.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="horizontal" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-preloader="enable">
+
+<head>
+    <meta charset="utf-8" />
+    <title> Edeklarant | Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
+    @include('layouts.head-css')
+</head>
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        @include('layouts.topbar')
+        @include('layouts.sidebar')
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            <div class="page-content">
+                <!-- Start content -->
+                <div class="container-fluid">
+                    @yield('content')
+                </div> <!-- content -->
+            </div>
+            @include('layouts.footer')
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Right content here -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+
+
+
+    @include('layouts.vendor-scripts')
+</body>
+
+</html>
---
+++ app-service-laravel/storage/framework/views/b1ec4ce69b7c0979ae421babbe00dab6.php
@@ -0,0 +1,5 @@
+
+<body>
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH C:\xampp_new\htdocs\edeklarant\app-service-laravel\resources\views/layouts/body.blade.php ENDPATH**/ ?>
