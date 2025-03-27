e3fe1fb77579f16d11cf5458910064edb3b9d432
charlieamer <amer_zavlan@live.com>
2025-03-27 11:54:55 +0100

    Delete unnecessary files

--- app-service-laravel/storage/framework/views/b1ec4ce69b7c0979ae421babbe00dab6.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH C:\xampp_new\htdocs\edeklarant\app-service-laravel\resources\views/layouts/body.blade.php ENDPATH**/ ?>
--- app-service-laravel/storage/framework/views/e8f6f6b526ba02d92f17dbc78093fcf1.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH C:\Users\DT User\edeklarant\app-service-laravel\resources\views/layouts/body.blade.php ENDPATH**/ ?>
