3e9a7be541436802add006151357eb8c28e22b41
kulasin <70981278+kulasin@users.noreply.github.com>
2025-04-29 20:44:39 +0200

    small-fixes

--- app-service-laravel/resources/views/analytics.blade.php
+++ app-service-laravel/resources/views/analytics.blade.php
@@ -878,140 +878,72 @@
                 <div class="card h-100 rounded-0">
                     <div class="card-body p-0">
                         <div class="p-3">
-                            <h6 class="text-muted mb-0 text-uppercase fw-semibold">Zadnje aktivnosti</h6>
+                            <h6 class="text-muted mb-0 text-uppercase fw-semibold">Zadnji proizvodi</h6>
                         </div>
                         <div data-simplebar style="max-height: 410px;" class="p-3 pt-0">
                             <div class="acitivity-timeline acitivity-main">
                                 <div class="acitivity-item d-flex">
                                     <div class="flex-shrink-0 avatar-xs acitivity-avatar">
-                                        <div class="avatar-title bg-success-subtle text-success rounded-circle shadow">
-                                            <i class="ri-shopping-cart-2-line"></i>
+                                        <div class="avatar-title bg-info-subtle text-info rounded-circle shadow">
+                                            <i class="ri-hashtag"></i>
                                         </div>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Purchase by James Price</h6>
-                                        <p class="text-muted mb-1">Product noise evolve smartwatch </p>
-                                        <small class="mb-0 text-muted">02:14 PM Today</small>
+                                        <h6 class="mb-1 lh-base">Ime proizvoda</h6>
+                                        <p class="text-muted mb-1">Tarifa </p>
+                                        <small class="mb-0 text-muted">22:14 Danas</small>
                                     </div>
                                 </div>
                                 <div class="acitivity-item py-3 d-flex">
                                     <div class="flex-shrink-0 avatar-xs acitivity-avatar">
-                                        <div class="avatar-title bg-danger-subtle text-danger rounded-circle shadow">
-                                            <i class="ri-stack-fill"></i>
+                                        <div class="avatar-title bg-info-subtle text-info rounded-circle shadow">
+                                            <i class="ri-hashtag"></i>
                                         </div>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Added new <span class="fw-semibold">style
-                                                collection</span></h6>
-                                        <p class="text-muted mb-1">By Nesta Technologies</p>
-                                        <div class="d-inline-flex gap-2 border border-dashed p-2 mb-2">
-                                            <a href="apps-ecommerce-product-details" class="bg-light rounded p-1">
-                                                <img src="{{ URL::asset('build/images/products/img-8.png') }}"
-                                                    alt="" class="img-fluid d-block" />
-                                            </a>
-                                            <a href="apps-ecommerce-product-details" class="bg-light rounded p-1">
-                                                <img src="{{ URL::asset('build/images/products/img-2.png') }}"
-                                                    alt="" class="img-fluid d-block" />
-                                            </a>
-                                            <a href="apps-ecommerce-product-details" class="bg-light rounded p-1">
-                                                <img src="{{ URL::asset('build/images/products/img-10.png') }}"
-                                                    alt="" class="img-fluid d-block" />
-                                            </a>
-                                        </div>
-                                        <p class="mb-0 text-muted"><small>9:47 PM Yesterday</small></p>
+                                        <h6 class="mb-1 lh-base">Ime proizvoda</h6>
+                                        <p class="text-muted mb-1">Tarifa </p>
+                                        <small class="mb-0 text-muted">22:14 Danas</small>
                                     </div>
                                 </div>
                                 <div class="acitivity-item py-3 d-flex">
-                                    <div class="flex-shrink-0">
-                                        <img src="{{ URL::asset('build/images/users/avatar-2.jpg') }}" alt=""
-                                            class="avatar-xs rounded-circle acitivity-avatar shadow">
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Natasha Carey have liked the products
-                                        </h6>
-                                        <p class="text-muted mb-1">Allow users to like products in your
-                                            WooCommerce store.</p>
-                                        <small class="mb-0 text-muted">25 Dec, 2021</small>
-                                    </div>
-                                </div>
-                                <div class="acitivity-item py-3 d-flex">
-                                    <div class="flex-shrink-0">
-                                        <div class="avatar-xs acitivity-avatar">
-                                            <div class="avatar-title rounded-circle bg-secondary shadow">
-                                                <i class="mdi mdi-sale fs-14"></i>
-                                            </div>
+                                    <div class="flex-shrink-0 avatar-xs acitivity-avatar">
+                                        <div class="avatar-title bg-info-subtle text-info rounded-circle shadow">
+                                            <i class="ri-hashtag"></i>
                                         </div>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Today offers by <a href="apps-ecommerce-seller-details"
-                                                class="link-secondary">Digitech
-                                                Galaxy</a></h6>
-                                        <p class="text-muted mb-2">Offer is valid on orders of Rs.500 Or
-                                            above for selected products only.</p>
-                                        <small class="mb-0 text-muted">12 Dec, 2021</small>
+                                        <h6 class="mb-1 lh-base">Ime proizvoda</h6>
+                                        <p class="text-muted mb-1">Tarifa </p>
+                                        <small class="mb-0 text-muted">22:14 Danas</small>
                                     </div>
                                 </div>
                                 <div class="acitivity-item py-3 d-flex">
-                                    <div class="flex-shrink-0">
-                                        <div class="avatar-xs acitivity-avatar">
-                                            <div class="avatar-title rounded-circle bg-danger-subtle text-danger shadow">
-                                                <i class="ri-bookmark-fill"></i>
-                                            </div>
+                                    <div class="flex-shrink-0 avatar-xs acitivity-avatar">
+                                        <div class="avatar-title bg-info-subtle text-info rounded-circle shadow">
+                                            <i class="ri-hashtag"></i>
                                         </div>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Favorite Product</h6>
-                                        <p class="text-muted mb-2">Esther James have Favorite product.
-                                        </p>
-                                        <small class="mb-0 text-muted">25 Nov, 2021</small>
+                                        <h6 class="mb-1 lh-base">Ime proizvoda</h6>
+                                        <p class="text-muted mb-1">Tarifa </p>
+                                        <small class="mb-0 text-muted">22:14 Danas</small>
                                     </div>
                                 </div>
                                 <div class="acitivity-item py-3 d-flex">
-                                    <div class="flex-shrink-0">
-                                        <div class="avatar-xs acitivity-avatar">
-                                            <div class="avatar-title rounded-circle bg-secondary shadow">
-                                                <i class="mdi mdi-sale fs-14"></i>
-                                            </div>
+                                    <div class="flex-shrink-0 avatar-xs acitivity-avatar">
+                                        <div class="avatar-title bg-info-subtle text-info rounded-circle shadow">
+                                            <i class="ri-hashtag"></i>
                                         </div>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Flash sale starting <span
-                                                class="text-primary">Tomorrow.</span></h6>
-                                        <p class="text-muted mb-0">Flash sale by <a href="javascript:void(0);"
-                                                class="link-secondary fw-medium">Zoetic Fashion</a></p>
-                                        <small class="mb-0 text-muted">22 Oct, 2021</small>
+                                        <h6 class="mb-1 lh-base">Ime proizvoda</h6>
+                                        <p class="text-muted mb-1">Tarifa </p>
+                                        <small class="mb-0 text-muted">22:14 Danas</small>
                                     </div>
                                 </div>
-                                <div class="acitivity-item py-3 d-flex">
-                                    <div class="flex-shrink-0">
-                                        <div class="avatar-xs acitivity-avatar">
-                                            <div class="avatar-title rounded-circle bg-info-subtle text-info shadow">
-                                                <i class="ri-line-chart-line"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Monthly sales report</h6>
-                                        <p class="text-muted mb-2"><span class="text-danger">2 days
-                                                left</span> notification to submit the monthly sales
-                                            report. <a href="javascript:void(0);"
-                                                class="link-warning text-decoration-underline">Reports
-                                                Builder</a></p>
-                                        <small class="mb-0 text-muted">15 Oct</small>
-                                    </div>
-                                </div>
-                                <div class="acitivity-item d-flex">
-                                    <div class="flex-shrink-0">
-                                        <img src="{{ URL::asset('build/images/users/avatar-3.jpg') }}" alt=""
-                                            class="avatar-xs rounded-circle acitivity-avatar shadow" />
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <h6 class="mb-1 lh-base">Frank Hook Commented</h6>
-                                        <p class="text-muted mb-2 fst-italic">" A product that has
-                                            reviews is more likable to be sold than a product. "</p>
-                                        <small class="mb-0 text-muted">26 Aug, 2021</small>
-                                    </div>
-                                </div>
+                                
+                           
                             </div>
                         </div>
 
@@ -1061,13 +993,10 @@
                                             class="float-end">(239)</span></a>
                                 </li>
                             </ol>
-                            <div class="mt-3 text-center">
-                                <a href="javascript:void(0);" class="text-muted text-decoration-underline">View all
-                                    Categories</a>
-                            </div>
+                           
                         </div>
-                        <div class="p-3">
-                            <h6 class="text-muted mb-3 text-uppercase fw-semibold">Products Reviews</h6>
+                        <div class="p-3 pt-0">
+                            <h6 class="text-muted mb-3 text-uppercase fw-semibold">Zadnji dobavljači</h6>
                             <!-- Swiper -->
                             <div class="swiper vertical-swiper" style="height: 250px;">
                                 <div class="swiper-wrapper">
@@ -1084,19 +1013,11 @@
                                                     <div class="flex-grow-1 ms-3">
                                                         <div>
                                                             <p class="text-muted mb-1 fst-italic text-truncate-two-lines">
-                                                                " Great product and looks great, lots of
-                                                                features. "</p>
-                                                            <div class="fs-11 align-middle text-warning">
-                                                                <i class="ri-star-fill"></i>
-                                                                <i class="ri-star-fill"></i>
-                                                                <i class="ri-star-fill"></i>
-                                                                <i class="ri-star-fill"></i>
-                                                                <i class="ri-star-fill"></i>
-                                                            </div>
+                                                                " MILKOS D.O.O "</p>
+                                                     
                                                         </div>
-                                                        <div class="text-end mb-0 text-muted">
-                                                            - by <cite title="Source Title">Force
-                                                                Medicines</cite>
+                                                        <div class="mb-0 text-muted">
+                                                           <i class="ri-map-pin-line text-info me-1"></i> <cite title="Source Title">Sarajevo</cite>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -1104,8 +1025,8 @@
                                         </div>
                                     </div>
                                     <div class="swiper-slide">
-                                        <div class="card border border-dashed shadow-none">
-                                            <div class="card-body">
+                                        <div class="card border border-dashed shadow-none" style="margin-bottom: 0; height: 500px!imporant;">
+                                            <div class="card-body" style="height: 500px!important;">
                                                 <div class="d-flex">
                                                     <div class="flex-shrink-0">
                                                         <img src="{{ URL::asset('build/images/users/avatar-3.jpg') }}"
