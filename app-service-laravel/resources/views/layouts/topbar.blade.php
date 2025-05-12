d3092e229941b8acabd95fbcda01e21fb9af3905
kulasin <70981278+kulasin@users.noreply.github.com>
2025-05-12 22:58:27 +0200

    dobrodosli-fix-2

--- app-service-laravel/resources/views/layouts/topbar.blade.php
+++ app-service-laravel/resources/views/layouts/topbar.blade.php
@@ -237,7 +237,7 @@
                     <div class="dropdown-menu dropdown-menu-end border">
                         <!-- item-->
                         <h6 class="dropdown-header">
-  Dobrodošli <script>document.write(JSON.parse(localStorage.getItem("user"))?.email);</script>!</h6>
+  Dobrodošli <script>document.write(JSON.parse(localStorage.getItem("user"))?.email);</script></h6>
                         <a class="dropdown-item" href="pages-profile"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Moj nalog</span></a>   
                         <a class="dropdown-item" href="pages-faqs"><i class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Pomoć</span></a>
                         <div class="dropdown-divider"></div>
