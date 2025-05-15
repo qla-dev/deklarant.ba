89a5136bc388c309a76d259477b2cfc41c6c7957
kulasin <70981278+kulasin@users.noreply.github.com>
2025-05-15 14:16:31 +0200

    meta-data-masters-fix

--- app-service-laravel/resources/views/layouts/master-without-nav.blade.php
+++ app-service-laravel/resources/views/layouts/master-without-nav.blade.php
@@ -3,10 +3,10 @@
 
     <head>
     <meta charset="utf-8" />
-    <title>@yield('title') | Velzon - Admin & Dashboard Template</title>
+    <title>@yield('title') | deklarant.ba - Napredni AI skener deklaracija</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="deklarant.ba - Napredni AI skener deklaracija" name="description" />
+    <meta content="qla.dev" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
         @include('layouts.head-css')
