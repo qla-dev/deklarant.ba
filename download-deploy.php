e53a2e07050fc12246fe9df66436ab6619660f7e
charlieamer <amer_zavlan@live.com>
2025-05-29 01:45:34 +0200

    logging

--- download-deploy.php
+++ download-deploy.php
@@ -71,7 +71,9 @@ if ($returnVar !== 0) {
     echo "Extracted successfully to $destination\n";
 }
 
+echo "Removing $escapedFile\n";
 exec("rm $escapedFile", $output, $returnVar);
 $fileWithoutExtension = pathinfo($tmpFile, PATHINFO_DIRNAME) . '/' . pathinfo($tmpFile, PATHINFO_FILENAME);
 $escapedFolder = escapeshellarg($fileWithoutExtension);
+echo "Removing folder $escapedFolder";
 exec("rm -rf $escapedFolder", $output, $returnVar);
