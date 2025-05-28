8f60c29f2ae28f0f057e4cd714857d55b8fec4d3
charlieamer <amer_zavlan@live.com>
2025-05-29 01:22:25 +0200

    clear routes on deploy

--- run-migrations.php
+++ run-migrations.php
@@ -1,4 +1,8 @@
 <?php
 chdir(__DIR__ . '/app-service-laravel');
 passthru('php artisan migrate --force 2>&1', $code);
+passthru('php artisan config:clear 2>&1', $code);
+passthru('php artisan cache:clear 2>&1', $code);
+passthru('php artisan view:clear 2>&1', $code);
+passthru('php artisan route:clear 2>&1', $code);
 http_response_code($code === 0 ? 200 : 500);
