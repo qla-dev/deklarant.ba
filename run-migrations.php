dfe8af914b138a8770d58b097941f1a55690f206
charlieamer <amer_zavlan@live.com>
2025-05-29 01:26:03 +0200

    run or fail for each command

--- run-migrations.php
+++ run-migrations.php
@@ -1,8 +1,18 @@
 <?php
 chdir(__DIR__ . '/app-service-laravel');
-passthru('php artisan migrate --force 2>&1', $code);
-passthru('php artisan config:clear 2>&1', $code);
-passthru('php artisan cache:clear 2>&1', $code);
-passthru('php artisan view:clear 2>&1', $code);
-passthru('php artisan route:clear 2>&1', $code);
-http_response_code($code === 0 ? 200 : 500);
+
+function runOrFail($command) {
+    passthru($command, $code);
+    if ($code !== 0) {
+        http_response_code(500);
+        exit("Command failed: $command\n");
+    }
+}
+
+runOrFail('php artisan migrate --force 2>&1');
+runOrFail('php artisan config:clear 2>&1');
+runOrFail('php artisan cache:clear 2>&1');
+runOrFail('php artisan view:clear 2>&1');
+runOrFail('php artisan route:clear 2>&1');
+
+http_response_code(200);
