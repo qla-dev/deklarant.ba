057a50f2b25e149b4362c49e63363fab88758876
kulasin <70981278+kulasin@users.noreply.github.com>
2025-05-31 15:12:31 +0200

    csrf-login-added

--- app-service-laravel/resources/views/layouts/master-without-nav.blade.php
+++ app-service-laravel/resources/views/layouts/master-without-nav.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="deklarant.ba - Napredni AI skener deklaracija" name="description" />
     <meta content="qla.dev" name="author" />
+       <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
         @include('layouts.head-css')
