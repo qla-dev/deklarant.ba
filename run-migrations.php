ebbc357e22603260abf3af36a257c9ccbd2cf0bd
charlieamer <amer_zavlan@live.com>
2025-06-04 23:08:46 +0200

    eh

--- run-migrations.php
+++ run-migrations.php
@@ -2,6 +2,7 @@
 chdir(__DIR__ . '/app-service-laravel');
 
 function runOrFail($command) {
+    $command = $command . ' 2>&1';
     passthru($command, $code);
     if ($code !== 0) {
         http_response_code(500);
@@ -9,13 +10,13 @@ function runOrFail($command) {
     }
 }
 
-runOrFail('php artisan migrate --force 2>&1');
-runOrFail('php artisan config:clear 2>&1');
-runOrFail('php artisan cache:clear 2>&1');
-runOrFail('php artisan view:clear 2>&1');
-runOrFail('php artisan route:clear 2>&1');
-runOrFail('php artisan config:cache 2>&1');
-runOrFail('php artisan route:cache 2>&1');
-runOrFail('php artisan view:cache 2>&1');
+runOrFail('php artisan migrate --force');
+runOrFail('php artisan config:clear');
+runOrFail('php artisan cache:clear');
+runOrFail('php artisan view:clear');
+runOrFail('php artisan route:clear');
+runOrFail('php artisan config:cache');
+runOrFail('php artisan route:cache');
+runOrFail('php artisan view:cache');
 
 http_response_code(200);
