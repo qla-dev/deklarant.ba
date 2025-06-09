c2dbdec2b00fed4c7dd05d7ae744e63fe386147f
kulasin <70981278+kulasin@users.noreply.github.com>
2025-06-09 18:56:30 +0200

    edit-fixes

--- app-service-laravel/resources/views/cijene-paketa.blade.php
+++ app-service-laravel/resources/views/cijene-paketa.blade.php
@@ -13,7 +13,7 @@
             <h4 class="fw-semibold fs-22">Odaberi paket koji odgovara tvojim potrebama <span class="fs-17" style="color:gray!important"><br>(odustani bez naknade bilo kada)</span></h4>
             <p class="text-muted fs-15 mb-3">
                 Provjeri kako se prenose preostala AI skeniranja u slučaju nadogradnje paketa, koliko dugo čuvamo podatke nakon isteka pretplate, kao i ostale korisne informacije u 
-                <a href="/faqs" class="text-info text-decoration-underline">Čestim pitanjima</a>.
+                <a href="/faqs" class="text-info text-decoration-underline">čestim pitanjima</a>.
             </p>
            
       </div>
